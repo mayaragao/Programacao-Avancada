32ad14838008a775053f4328ee62c184b33129a3
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-14 19:00:53 -0300

    Created front-end directory

---
+++ reactJS/mvp-mae/README.md
@@ -0,0 +1,1 @@
+# Front-end development of Advanced Programming project
---
+++ reactJS/mvp-mae/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+function App() {
+  return (
+    <div>
+      <p>AAAA</p>
+    </div>
+  );
+}
+
+export default App;
