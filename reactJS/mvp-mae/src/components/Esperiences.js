ebc10482e77f7321bc4c6d16b6265ad681099da2
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-15 14:53:38 -0300

    initial template for experiences div, requires API's correct names

--- reactJS/mvp-mae/src/components/Esperiences.js
+++ reactJS/mvp-mae/src/components/Esperiences.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Experience from "./Experience";
+
+// Here comes the array of objects.
 const Experiences = ({ experiences }) => {
   return (
     <div className="experiences">
       {experiences.map((experience) => {
-        return <Experience key={experience.id} {...experience} />;
+        return <Experience key={experience.cd_exp} {...experience} />;
       })}
     </div>
   );
---
+++ reactJS/mvp-mae/src/components/Experience.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Experience = (props) => {
+    // ATENCAO, DEPENDENDO DO JJSON, MUDAR OS SEGUINTES NOMES NA DESTRUICAo do objeto
+    const {name_usuario,name_usuario_childm,description,comments,likes}
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Experience
