90db8d755f46b3f3c2af02e5696ea182e87ab344
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-16 19:36:08 -0300

    added some commentaries to get rid of dummy text

--- reactJS/mvp-mae/src/components/Card_experience.js
+++ reactJS/mvp-mae/src/components/Card_experience.js
@@ -12,21 +12,21 @@ import {
   CardDeck,
   Container,
 } from "react-bootstrap";
-import { render } from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Cards = (props) => {
   return (
     <Container className="card-container mb-4 mt-4">
       <CardDeck>
-        <Card id="card1" className="cardzin mt-4">
+        <Card className="cardzin mt-4">
           <Card.Body>
             <Card.Title>Mayara</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
-              mae do zezin
+              mae do Zezin
             </Card.Subtitle>
             <Card.Text>
-              Conheci um pai solteirao maravilhoso, nome dele e Pedro ( ͡° ͜ʖ ͡°).
-              Muito obrigado Mom's help por achar minha alma gemea S2
+              Conheci um pai solteirao maravilhoso. Muito obrigado Mom's help
+              por achar minha alma gemea ♥
             </Card.Text>
           </Card.Body>
         </Card>
@@ -37,53 +37,63 @@ const Cards = (props) => {
               mae do Pessoa
             </Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Dei a luz ao Pessoa mas depois que ele conheceu o Raul atraves do
+              app, tudo que ele faz é falar sobre backend. O app cumpre o que
+              promete!
             </Card.Text>
           </Card.Body>
         </Card>
         <Card className="cardzin mt-4" style={{ width: "18rem" }}>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Pedro</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              pai da Kira
+            </Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Sinceramente comecei a usar esse app apenas para encontrar mães
+              solteiras. Atualmente, nao apenas encontrei uma parceira, como uma
+              otima madrasta para a minha filha.
             </Card.Text>
           </Card.Body>
         </Card>
       </CardDeck>
 
       <CardDeck>
-        <Card id="card1" className="cardzin mt-4">
+        <Card className="cardzin mt-4">
           <Card.Body>
-            <Card.Title>Mayara</Card.Title>
+            <Card.Title>Miceli</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
-              mae do zezin
+              pai do Micelio
             </Card.Subtitle>
             <Card.Text>
-              Conheci um pai solteirao maravilhoso, nome dele e Pedro ( ͡° ͜ʖ ͡°).
-              Muito obrigado Mom's help por achar minha alma gemea S2
+              Micelio era viciado em Docker, Flask, Microsserviços, NextJS e em
+              puxar serviços no Fiverr. Graças ao aplicativo, fomos capazes de
+              interagir com outra familia e Micelio finalmente conseguiu
+              desenvolver sua vida social.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card className="cardzin mt-4" style={{ width: "18rem" }}>
           <Card.Body>
-            <Card.Title>Ana</Card.Title>
+            <Card.Title>Pessoa</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
-              mae do Pessoa
+              mae do Backend
             </Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A aplicacao superiou minhas expectativas. Entretanto, ha muitos
+              anuncios de fraldas a 40 reais, assim nao da.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card className="cardzin mt-4" style={{ width: "18rem" }}>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Maria</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              mae do Alvaro
+            </Card.Subtitle>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Comecei a usar a aplicacao para fazer um relatorio seguindo os
+              moldes da sociedade brasileira de computacao, esta dificil.
             </Card.Text>
           </Card.Body>
         </Card>
--- reactJS/mvp-mae/src/components/Topo.js
+++ reactJS/mvp-mae/src/components/Topo.js
@@ -17,7 +17,11 @@ const Topo = () => {
           <h1>Mom's Help</h1>
 
           <br />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate delectus, beatae eos quisquam earum libero sunt veniam aliquid impedit recusandae!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            delectus, beatae eos quisquam earum libero sunt veniam aliquid
+            impedit recusandae!
+          </p>
         </div>
         <img className="imgtopo" alt="img_topo" src={imagemTopo} />
       </div>
