ab454693c3bd9820b872508b1b7c59797dcdd969
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-16 19:43:29 -0300

    getting rid of unnecessary stuff

--- reactJS/mvp-mae/src/components/Topo.js
+++ reactJS/mvp-mae/src/components/Topo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
+
 import Header from "./Header";
+
 import imagemTopo from "./images/iconeTopo.svg";
 
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,7 +17,11 @@ const Topo = () => {
           <h1>Mom's Help</h1>
 
           <br />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate delectus, beatae eos quisquam earum libero sunt veniam aliquid impedit recusandae!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            delectus, beatae eos quisquam earum libero sunt veniam aliquid
+            impedit recusandae!
+          </p>
         </div>
         <img className="imgtopo" alt="img_topo" src={imagemTopo} />
       </div>
