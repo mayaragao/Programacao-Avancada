18eb317feb3fd065eb5726583b5cd3a6e9756aef
Ana Canto <ana.lucia.canto@poli.ufrj.br>
2021-05-16 13:33:48 -0300

    Adicionando página para imagens.

--- reactJS/mvp-mae/src/pages/Login/Login.js
+++ reactJS/mvp-mae/src/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col} from "react-bootstrap";
-import { RightContainer } from './styles';
+import { RightContainer} from './styles';
+import LoginImage from '../../assets/images/login_image.svg'
 
 
 const LoginPage = () => {
@@ -8,7 +9,9 @@ const LoginPage = () => {
     <Container>
         <Row>
           <Col></Col>
-          <Col><RightContainer/></Col>
+          <Col><RightContainer>        
+            <img src={LoginImage} alt="Imagem de Login" id="loginImage" />
+          </RightContainer></Col>
         </Row>       
     </Container>)
  
--- reactJS/mvp-mae/src/pages/Login/styles.js
+++ reactJS/mvp-mae/src/pages/Login/styles.js
@@ -4,6 +4,15 @@ export const RightContainer = styled.div`
   background-color: #90AB9B;
   width: 50vw;
   height: 100vh;
-  
+  margin-right: -9.4vw;
+  display: flex;
+  justify-content: center;
+
+  #loginImage{
+  width: 35vw;
+  background-color: #90AB9B
+  }
 `;
+
+
 
