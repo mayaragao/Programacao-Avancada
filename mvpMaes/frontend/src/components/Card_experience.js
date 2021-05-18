1f5ea68213407bef74ed9415b7fb402ff128afd5
mayaragao <mayaraaragao@poli.ufrj.br>
2021-05-17 21:40:04 -0300

    Listando experiencias na home

--- mvpMaes/frontend/src/components/Card_experience.js
+++ mvpMaes/frontend/src/components/Card_experience.js
@@ -1,105 +1,148 @@
 import React, { Component, useEffect, useState } from "react";
-import {
-  Row,
-  Col,
-  Alert,
-  Button,
-  OverlayTrigger,
-  Tooltip,
-  Table,
-  Tab,
-  Card,
-  CardDeck,
-  Container,
-} from "react-bootstrap";
+import { Row, Col, Alert, Button, OverlayTrigger, Tooltip, Table, Tab, Card, CardDeck, Container } from "react-bootstrap";
+
+import ExperienceService from "../services/experienceService";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiceFive, faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const Cards = (props) => {
-  return (
-    <Container className="card-container">
-      <CardDeck>
-        <Card className="cardzin mt-4">
-          <Card.Body>
-            <Card.Title>Mayara</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              mae do Zezin
-            </Card.Subtitle>
-            <Card.Text>
-              Conheci um pai solteirao maravilhoso. Muito obrigado Mom's help
-              por achar minha alma gemea ♥
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="cardzin mt-4" style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Ana</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              mae do Pessoa
-            </Card.Subtitle>
-            <Card.Text>
-              Dei a luz ao Pessoa mas depois que ele conheceu o Raul atraves do
-              app, tudo que ele faz é falar sobre backend. O app cumpre o que
-              promete!
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="cardzin mt-4" style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Pedro</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              pai da Kira
-            </Card.Subtitle>
-            <Card.Text>
-              Sinceramente comecei a usar esse app apenas para encontrar mães
-              solteiras. Atualmente, nao apenas encontrei uma parceira, como uma
-              otima madrasta para a minha filha.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+class Cards extends Component {
+  constructor(props) {
+    super(props);
 
-      <CardDeck>
-        <Card className="cardzin mt-4">
-          <Card.Body>
-            <Card.Title>Miceli</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              pai do Micelio
-            </Card.Subtitle>
-            <Card.Text>
-              Micelio era viciado em Docker, Flask, Microsserviços, NextJS e em
-              puxar serviços no Fiverr. Graças ao aplicativo, fomos capazes de
-              interagir com outra familia e Micelio finalmente conseguiu
-              desenvolver sua vida social.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="cardzin mt-4" style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Pessoa</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              mae do Backend
-            </Card.Subtitle>
-            <Card.Text>
-              A aplicação superou minhas expectativas. Entretanto, há muitos
-              anúncios de fraldas a 40 reais, assim não dá.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className="cardzin mt-4" style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Maria</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              mae do Alvaro
-            </Card.Subtitle>
-            <Card.Text>
-              Comecei a usar a aplicacao para fazer um relatorio seguindo os
-              moldes da sociedade brasileira de computacao, esta dificil.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
-    </Container>
-  );
-};
+    this.SearchExperiences = this.SearchExperiences.bind(this);
+
+    this.state = {
+      card_experiences1: [],
+      card_experiences2: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.SearchExperiences();
+  }
+
+  async SearchExperiences() {
+    var list_experiences1 = [];
+    var list_experiences2 = [];
+    var data_experience = null;
+
+    try {
+      const response_experiences = await ExperienceService.getExperiences();
+      console.log("conseguiu!", response_experiences);
+      for (const [idx, obj] of response_experiences.data.entries()) {
+        data_experience = {
+          _id: obj._id,
+          username: obj.username,
+          name_usuario_child: "criança",
+          description: obj.description,
+          numLikes: obj.numLikes,
+          comments: obj.comments,
+        };
+
+        if (idx < 3) {
+          list_experiences1.push(data_experience);
+        } else if (idx < 6) {
+          list_experiences2.push(data_experience);
+        } else {
+          break;
+        }
+      }
+    } catch (err_experiences) {
+      console.log(err_experiences);
+    }
+
+    this.setState({ card_experiences1: list_experiences1, card_experiences2: list_experiences2 });
+  }
+
+  render() {
+    return (
+      <div className="card-container">
+        <CardDeck>
+          {this.state.card_experiences1.map((experience, i) => (
+            <Card className="cardzin mt-4">
+              <Card.Body>
+                <Card.Title>
+                  {experience.username} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <small>
+                    {experience.numLikes}&nbsp;
+                    <FontAwesomeIcon icon={faHeart} size="sm" />
+                  </small>{" "}
+                </Card.Title>
+                <Card.Text>{experience.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </CardDeck>
+
+        <CardDeck>
+          {this.state.card_experiences2.map((experience, i) => (
+            <Card className="cardzin mt-4">
+              <Card.Body>
+                <Card.Title>
+                  {experience.username} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <small>
+                    {experience.numLikes}&nbsp;
+                    <FontAwesomeIcon icon={faHeart} size="sm" />
+                  </small>{" "}
+                </Card.Title>
+                <Card.Text>{experience.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </CardDeck>
+
+        {/*
+        <CardDeck>
+          <Card className="cardzin mt-4">
+            <Card.Body>
+              <Card.Title>Mayara</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">mae do Zezin</Card.Subtitle>
+              <Card.Text>Conheci um pai solteirao maravilhoso. Muito obrigado Mom's help por achar minha alma gemea ♥</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cardzin mt-4" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Ana</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">mae do Pessoa</Card.Subtitle>
+              <Card.Text>Dei a luz ao Pessoa mas depois que ele conheceu o Raul atraves do app, tudo que ele faz é falar sobre backend. O app cumpre o que promete!</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cardzin mt-4" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Pedro</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">pai da Kira</Card.Subtitle>
+              <Card.Text>Sinceramente comecei a usar esse app apenas para encontrar mães solteiras. Atualmente, nao apenas encontrei uma parceira, como uma otima madrasta para a minha filha.</Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+
+        <CardDeck>
+          <Card className="cardzin mt-4">
+            <Card.Body>
+              <Card.Title>Miceli</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">pai do Micelio</Card.Subtitle>
+              <Card.Text>Micelio era viciado em Docker, Flask, Microsserviços, NextJS e em puxar serviços no Fiverr. Graças ao aplicativo, fomos capazes de interagir com outra familia e Micelio finalmente conseguiu desenvolver sua vida social.</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cardzin mt-4" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Pessoa</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">mae do Backend</Card.Subtitle>
+              <Card.Text>A aplicação superou minhas expectativas. Entretanto, há muitos anúncios de fraldas a 40 reais, assim não dá.</Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className="cardzin mt-4" style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Maria</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">mae do Alvaro</Card.Subtitle>
+              <Card.Text>Comecei a usar a aplicacao para fazer um relatorio seguindo os moldes da sociedade brasileira de computacao, esta dificil.</Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+         */}
+      </div>
+    );
+  }
+}
 
 export default Cards;
