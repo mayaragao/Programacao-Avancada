fa7227c7e7d2e94edabcdf62e6460683efcea4e0
luis.pessoa <pessoa@poli.ufrj.br>
2021-06-13 21:07:33 -0300

    Route to upload an image is finally working properly

--- mvpMaes/backend/routes/images.js
+++ mvpMaes/backend/routes/images.js
@@ -1,9 +1,39 @@
 const router = require('express').Router();
 let Image = require('../models/images.model');
 
+
 const multer = require('multer');
-const upload = multer({dest:'uploads/'});
+
+//Implement a storage strategy
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb){
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  //reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    //Accept a file
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }  
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+  fileSize: 1024*1024*5
+  },
+  fileFilter: fileFilter
+});
 const fs = require('fs');
+
+
 
 //Busca experiencia dado o ID da experiencia
 router.route('/upload').post(upload.single('userImage'), (req, res) => {
@@ -20,22 +50,61 @@ router.route('/upload').post(upload.single('userImage'), (req, res) => {
   size = req.file.size
   
   const newImage = new Image({username, originalname, mimetype, destination, filename, path, size})
-  console.log(newImage)
-
-  newImage.save()
-  .then(() => res.status(201).json('message: success'))
+  console.log(`My new image is: ${newImage}`)
+  var query = {'username':req.user.username}
+  
+  Image.findOneAndDelete(query)
+  .then(image => {
+    console.log(`My found image ${image}`)
+    if (image !== null) {
+      fs.unlink(image.path, (err) => {
+        if (err) {
+            throw err;
+        }  
+        console.log("File is deleted.");
+      });
+    }
+    
+    //---------------
+    newImage.save()
+  .then(() => {
+    res.status(201).json('message: success')
+  })
+  .catch(() => {
+    console.log(err)
+      fs.unlink(path, (err) => {
+        if (err) {
+            throw err;
+        }    
+        console.log("File is deleted.");
+      });
+    res.status(400).json('Error: Something went wrong with your request.');
+  })
+    //---------------
+  })
   .catch(err => {
       console.log(err)
       fs.unlink(path, (err) => {
         if (err) {
             throw err;
-        }
-    
+        }    
         console.log("File is deleted.");
       });
       res.status(400).json('Error: Something went wrong with your request.');
-    });
+    });  
   });
+
+
+  router.route('/download').get((req, res) => {
+    console.log('Download image called.')
+      Image.find({username:req.user.username})
+      .then(imageFetched => {
+        res.sendFile(imageFetched.path)
+      })
+      .catch(imageFetched => {
+        res.send.status(403).json({message: 'failed', error: 'user unidentified'})
+      })
+    });
 
 module.exports = router;
 
